a56d828bf0074f63b9eff47da81595a3952b77c4
DanielGenesis <Danielokp328@gmail.com>
2025-08-18 15:29:19 +0100

    Add taskSix and taskSeven Java files
    Introduces two new Java files, taskSix.java and taskSeven.java, each demonstrating array sorting and output. Both files contain similar logic for sorting an integer array and printing results, though the print statements may contain errors.

---
+++ JAVA/taskSeven.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class taskSeven{
+public static void main(String[] args) {
+
+int[] myArray = {1, 3, 4, 7, 5 };
+Arrays.sort(myArray);
+String newArray = Arrays.toString(myArray);
+System.out.println(newArray(newArray(0));
+}
+}
---
+++ JAVA/taskSix.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class taskSix{
+public static void main(String[] args) {
+
+int[] myArray = {1, 3, 4, 7, 5 };
+Arrays.sort(myArray);
+String newArray = Arrays.toString(myArray);
+System.out.println(newArray(newArray.length()-1));
+}
+}
+
+
+
